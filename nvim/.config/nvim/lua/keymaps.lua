83f788d7e73f7cf33d598e07af133b3eddce4f52
Carl Woodward <carl@88cartell.com>
2021-11-07 15:39:50 +1100

    chore: add key mappings

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -10,6 +10,7 @@ map('i', '<leader>s', '<C-c>:w<CR>', default_opts)
 map('n', '<leader>w', ':w<CR>', default_opts)
 map('i', '<leader>w', '<C-c>:w<CR>', default_opts)
 
+map('n', '<leader>n', ':nohlsearch<CR>', default_opts)
 map('n', '<leader>t', ':Telescope git_files disable_devicons=true<CR>', default_opts)
 map('n', '<leader>a', ':Telescope live_grep disable_devicons=true<CR>', default_opts)
 map('n', '<leader>b', ':Telescope buffers disable_devicons=true<CR>', default_opts)
@@ -18,4 +19,4 @@ map('n', '<leader>gc', ':Telescope git_commits disable_devicons=true<CR>', defau
 map('n', '<leader>ld', ':Telescope lsp_definitions disable_devicons=true<CR>', default_opts)
 map('n', '<leader>lr', ':Telescope lsp_references disable_devicons=true<CR>', default_opts)
 map('n', '<leader>le', ':Telescope lsp_document_diagnostics disable_devicons=true<CR>', default_opts)
-map('n', '-', ':Telescope file_browser disable_devicons=true<CR>', default_opts)
+map('n', '-', ':Telescope file_browser disable_devicons=true cwd=%:p:h<CR>', default_opts)
